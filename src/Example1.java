c5e54fc28af7d858aebbd4756ddda59556c6182d
ANA KHASANAH <ana.khasanah@capita.com>
2023-04-04 11:28:56 +0100

    Loops exercises

---
+++ src/Example1.java
@@ -0,0 +1,16 @@
+public class Example1 {
+    public static void main(String[] args) {
+        // Loop: a way for programmer to repeat the code as long as the condition is true
+        // for loop
+
+        // repeat 5 times
+        for (int i=0; i < 5; i++) {
+            // body of for loop, inside this will be repeated if condition is true
+            System.out.println("Kick the enemy!");
+        }
+
+        // this is outside the for loop
+
+
+    }
+}
