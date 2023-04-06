014fcafdf3a4d929a3bd66c50a62786cd069e6a0
ANA KHASANAH <ana.khasanah@capita.com>
2023-04-06 11:27:46 +0100

    Loops exercises, Q1-Q3

---
+++ src/TimesTable.java
@@ -0,0 +1,18 @@
+// 1. import Scanner
+
+public class TimesTable {
+    public static void main(String[] args) {
+        // 2. create object scanner
+
+        // 3. print "Enter a number to display the times table:"
+
+        // 4. retain user input using .nextInt()
+
+
+        // create for loop to print times table 1*userAnswer to 12*userAnswer
+        for (int i=1; i<=12; i++){
+            System.out.println(i);
+        }
+
+    }
+}
